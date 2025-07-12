399602aa7c4e8e07112d9154f84b7af15c9726e4
Nabeel87 <nabilsabir786@gmail.com>
2025-07-12 13:15:23 +0500

    Update GlobalState.js
    Create the provider for the global context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,7 +1,7 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useState } from "react";
 
 const initialSate = {
-    transaction: [
+    transactions: [
         { id: 1, description: 'Income 1', transactionAmount: 10000 },
         { id: 2, description: 'Income 2', transactionAmount: 20000 },
         { id: 3, description: 'Salery 1', transactionAmount: -5000 },
@@ -11,3 +11,18 @@ const initialSate = {
 
 //create the global context
 export const GlobalContext = createContext(initialSate);
+
+//create the provider for the global context
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialSate);
+
+    return(
+        <GlobalContext.Provider value={
+            {
+                transactions: state.transactions
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
