e00631c3d98ac906bde2c57f516a8ad5037806fd
Nabeel87 <nabilsabir786@gmail.com>
2025-07-12 13:05:34 +0500

    Update GlobalState.js
    initial state value

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,1 +1,10 @@
-import React, {createContext, useReducer} from "react";
+import React, { createContext, useReducer } from "react";
+
+const initialSate = {
+    transaction: [
+        { id: 1, description: 'Income 1', transactionAmount: 10000 },
+        { id: 2, description: 'Income 2', transactionAmount: 20000 },
+        { id: 3, description: 'Salery 1', transactionAmount: -5000 },
+        { id: 4, description: 'Salery 2', transactionAmount: -10000 }
+    ]
+}
