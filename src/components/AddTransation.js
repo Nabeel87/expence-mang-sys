57094f466bf6cc31a233ad6a6efb015e4c9c239d
Nabeel87 <nabilsabir786@gmail.com>
2025-07-12 11:40:07 +0500

    ui add transaction

--- src/components/AddTransation.js
+++ src/components/AddTransation.js
@@ -4,6 +4,21 @@ export const AddTransation = () => {
   return (
     <div>
         <h3>Add New Transation</h3>
+        <form>
+          <div className="form-control">
+            <label htmlFor="description">
+
+            </label>
+            <input type="text" placeholder="Details of Transaction" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="transactionamount">
+
+            </label>
+            <input type="text" placeholder="Amount of transaction" />
+          </div>
+          <button className="btn"> Add Transaction</button>
+        </form>
     </div>
   )
 }
