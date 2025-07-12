a8be0a71ca15b12af134b633dddb74bda432a852
Nabeel87 <nabilsabir786@gmail.com>
2025-07-12 11:47:35 +0500

    Update AddTransation.js
    use useStete in input fields

--- src/components/AddTransation.js
+++ src/components/AddTransation.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransation = () => {
+
+  const [description, setDescription] = useState('');
+  const [transactionAmount, setTransactionAmount] = useState(0);
   return (
     <div>
-        <h3>Add New Transation</h3>
-        <form>
-          <div className="form-control">
-            <label htmlFor="description">
+      <h3>Add New Transation</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="description">
 
-            </label>
-            <input type="text" placeholder="Details of Transaction" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="transactionamount">
+          </label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            placeholder="Details of Transaction" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="transactionamount">
 
-            </label>
-            <input type="text" placeholder="Amount of transaction" />
-          </div>
-          <button className="btn"> Add Transaction</button>
-        </form>
+          </label>
+          <input
+            type="number"
+            id="transactionamount"
+            value={transactionAmount}
+            placeholder="Amount of transaction" />
+        </div>
+        <button className="btn"> Add Transaction</button>
+      </form>
     </div>
   )
 }
